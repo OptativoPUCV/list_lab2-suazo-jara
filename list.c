fdcab6496ef2f48ff95ac6822510545e843c72ab
Replit user <replituser@example.com>
2021-03-25 23:48:22 +0000

    new try

--- list.c
+++ list.c
@@ -116,7 +116,9 @@ void * popBack(List * list) {
 
 //EJERCICIO 6
 void * popCurrent(List * list) {
-    return NULL;
+  void* aux = list->current->data;
+  //list->current->prev = list->current;
+  return aux;
 }
 
 void cleanList(List * list) {
